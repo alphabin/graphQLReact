57655b6cceade28e6656f085382b4307071efda5
alphabin <chatura@alphacodelab.com>
2022-02-06 23:01:14 -0800

    front end test

---
+++ hackernews-react-apollo/src/components/Link.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Link = ({ link: { description, url } }) => {
+    return (
+        <div>
+            <div>
+                {description} ({url})
+            </div>
+        </div>
+    );
+};
+
+export default Link;
